11064f9b7d10ace4ccf8fd32fdae259c3588f691
TaurusVB <114020229+TaurusVB@users.noreply.github.com>
2024-01-16 10:36:08 +0200

    CREATE LogoUploader

--- src/app/components/InputField.tsx
+++ src/app/components/InputField.tsx
@@ -3,8 +3,7 @@
 import React from "react";
 import { Field } from "formik";
 
-export interface InputFieldProps
-  extends React.InputHTMLAttributes<HTMLInputElement> {
+interface InputFieldProps extends React.InputHTMLAttributes<HTMLInputElement> {
   label?: string;
 }
 
